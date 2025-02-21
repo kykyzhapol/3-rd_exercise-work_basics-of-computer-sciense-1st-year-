b288f05f26d4c10b640c342c41a8a1bd9c8639bc
Sergey Chirkov <s.chirkov1@g.nsu.ru>
2025-02-21 19:05:03 +0700

    2/13 is ready

--- 3rd practice task (draft).py
+++ 3rd practice task (draft).py
@@ -1,3 +1,9 @@
+
 #1st
 price = input()
 print(price[3])
+
+#2nd
+time = int(input())
+print(f'{time//60**2} часов {int((time/60**2 - time//60**2)*60)} минут',
+      f'{round((((time/60**2 - time//60**2)*60)-int((time/60**2 - time//60**2)*60))*60)} секунд')
