33138180133e5acce20e32371d6bbef257986ebb
Sergey Chirkov <s.chirkov1@g.nsu.ru>
2025-02-24 01:54:22 +0700

    6/13 is ready

--- 3rd practice task (draft).py
+++ 3rd practice task (draft).py
@@ -1,4 +1,3 @@
-
 #1st
 price = input()
 print(price[3])
@@ -7,3 +6,24 @@ print(price[3])
 time = int(input())
 print(f'{time//60**2} часов {int((time/60**2 - time//60**2)*60)} минут',
       f'{round((((time/60**2 - time//60**2)*60)-int((time/60**2 - time//60**2)*60))*60)} секунд')
+
+#3rd
+num = int(input())
+print(num%(num//2))
+
+import math
+
+#4st
+lst = list(map(int, input().split()))
+print(f'{math.floor((lst[0]*100+lst[1])*lst[2]/100)} руб. {(lst[0]*100+lst[1])*lst[2]%100} коп.')
+#5th
+n = int(input())
+print("(\___/) "*n, "(='.'=) "*n, '(")_(") '*n, sep='\n')
+
+#6th
+raw = input('Enter number:')
+try:
+      num = int(raw)
+      print(num)
+except:
+      print(raw)
