02b2f832fcb4680fddd2cdd28d8a32100648cebb
Sergey Chirkov <s.chirkov1@g.nsu.ru>
2025-02-28 01:50:31 +0700

    10/13 is ready

--- 3rd practice task (draft).py
+++ 3rd practice task (draft).py
@@ -21,9 +21,46 @@ n = int(input())
 print("(\___/) "*n, "(='.'=) "*n, '(")_(") '*n, sep='\n')
 
 #6th
+K = int(input())
+N = int(input())
+R = int(input())
+print(str(K*R)*N)
+
+#7th
 raw = input('Enter number:')
 try:
       num = int(raw)
       print(num)
 except:
       print(raw)
+
+#8th
+import math
+#cos(α) = (b2 + c2 - a2) / (2 * b * c)
+A = int(input())
+B = int(input())
+C = int(input())
+print(
+      math.degrees(math.acos(((B**2)+(C**2)-(A**2))/(2*B*C))),
+      math.degrees(math.acos(((A**2)+(C**2)-(B**2))/(2*A*C))),
+      math.degrees(math.acos(((B**2)+(A**2)-(C**2))/(2*B*A)))
+)
+
+#9th
+ATT = int(input())
+COMP = int(input())
+YDS = int(input())
+TD = int(input())
+INT = int(input())
+a = ((COMP/ATT)-0.3)*5
+b = ((YDS/ATT)-3)*0.25
+c = (TD/ATT)*20
+d = 2.375-(INT/ATT)*25
+if a<0 or b<0 or c<0 or d<0:
+    print(0)
+else:
+    print(((a+b+c+d)/6)*100)
+
+#10th
+num = list(map(int, input().split()))
+print(int((num[0]/num[1]-num[0]//num[1])+(num[1]/num[0]-num[1]//num[0])+1))
